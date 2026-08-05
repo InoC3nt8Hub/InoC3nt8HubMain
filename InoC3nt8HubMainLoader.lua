d289c5e2200fc243edd3af4c92bcfde10a207c85
InoC3nt8Hub <xinngchao17@gmail.com>
2026-08-06 04:36:56 +0630

    Update script URL for game 11765763028

--- InoC3nt8HubMainLoader.lua
+++ InoC3nt8HubMainLoader.lua
@@ -7,7 +7,7 @@ local LocalPlayer = Players.LocalPlayer
 
 local SupportedGames = {
 
-    [11765763028] = "https://raw.githubusercontent.com/InoC3nt8Hub/InoC3nt8HubScripts/refs/heads/main/ReignFallScriptUpdateV3.lua.txt",
+    [11765763028] = "https://raw.githubusercontent.com/InoC3nt8Hub/InoC3nt8HubScripts/refs/heads/main/ReignFallScriptNewUpdated.lua.txt",
 
     [79399613552695] = "https://raw.githubusercontent.com/InoC3nt8Hub/InoC3nt8HubScripts/refs/heads/main/SomTum_AnomalyScript.lua.txt",
 
